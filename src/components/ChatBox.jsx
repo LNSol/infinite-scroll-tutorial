730a574f33d84c561616abf17f17b9022350ebfc
EunhaLim <dmsgk1559@gmail.com>
2022-12-19 13:14:47 +0900

    variable size item scroll 야호

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,45 +1,58 @@
-import { useEffect, useRef } from 'react';
-import { VariableSizeList as List } from 'react-window';
+import { useEffect, useRef, memo } from 'react';
+import { VariableSizeList as List, areEqual } from 'react-window';
+import { LoremIpsum } from 'lorem-ipsum';
 import ChatMsg from './ChatMsg';
+
+const lorem = new LoremIpsum({
+  wordsPerSentence: {
+    max: 32,
+    min: 4,
+  },
+});
 
 const Msgs = Array.from({ length: 100 }, (_, idx) => ({
   id: idx + 1,
-  content: `Message ${idx + 1}`,
+  content: lorem.generateSentences(1),
 }));
 
 const ChatBox = () => {
   const listRef = useRef({});
   const rowHeights = useRef({});
 
+  console.log('render');
+
   const getRowHeight = (index) => {
     return rowHeights.current[index] || 50;
   };
   const setRowHeight = (index, size) => {
-    // listRef.current.resetAfterIndex(0);
+    listRef.current.resetAfterIndex(0);
     rowHeights.current = { ...rowHeights.current, [index]: size };
-    console.log('rowHeights.current > ', rowHeights.current);
   };
 
-  const Row = ({ index, style }) => {
-    const rowRef = useRef(null);
+  const Row = memo(({ index, style }) => {
+    const rowRef = useRef({});
 
     useEffect(() => {
       if (rowRef.current) {
+        console.log('>> ', rowRef.current.clientHeight);
         setRowHeight(index, rowRef.current.clientHeight);
       }
     }, [rowRef.current]);
 
     return (
-      <div ref={rowRef} style={style}>
-        <ChatMsg msg={Msgs[index]} />
+      <div style={style}>
+        <div ref={rowRef}>
+          <ChatMsg msg={Msgs[index]} />
+        </div>
       </div>
     );
-  };
+  }, areEqual);
 
   return (
     <div>
       <h4>ChatBox</h4>
       <List
+        style={{ border: '1px solid black' }}
         width={400}
         height={600}
         itemCount={Msgs.length}
--- src/components/ChatMsg.jsx
+++ src/components/ChatMsg.jsx
@@ -1,12 +1,8 @@
 import { memo } from 'react';
+
 const ChatMsg = ({ msg }) => {
   return (
-    <div
-      style={{
-        height: 50 + Math.round(Math.random() * 50),
-        border: '1px solid red',
-      }}
-    >
+    <div style={{ border: '1px solid red' }}>
       {msg.id}: {msg.content}
     </div>
   );
